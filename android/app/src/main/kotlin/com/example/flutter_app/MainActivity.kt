0c766409ca33d66c3bf359b6f20d20c66c6cfd8b
Nandak99 <54415494+Nandak99@users.noreply.github.com>
2022-03-10 20:14:17 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.flutter_app
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+
+import io.flutter.embedding.android.FlutterActivity
+
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
